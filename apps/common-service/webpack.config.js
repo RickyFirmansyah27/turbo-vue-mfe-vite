60a1cfb2b1fbd9f4bf77fe6f66ba2aed12dbbc4d
RickyFirmansyah27 <ricky_firmansyah@teknokrat.ac.id>
2025-05-28 21:49:42 +0700

    add webpack version

--- apps/common-service/webpack.config.js
+++ apps/common-service/webpack.config.js
@@ -1,37 +1,74 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { VueLoaderPlugin } = require('vue-loader');
 const { ModuleFederationPlugin } = require('webpack').container;
 const path = require('path');
 
 module.exports = {
-  mode: 'production', // Ubah jadi production
   entry: './src/index.js',
   output: {
-    publicPath: 'http://localhost:6969/',
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
-    clean: true,
+    publicPath: 'http://localhost:6969/'
   },
   devServer: {
-    port: 6969,
-    static: {
-      directory: path.join(__dirname, 'dist'),
-    },
+    static: path.join(__dirname, 'dist'),
+    port: 8082,
+    hot: true,
     headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
+      'Access-Control-Allow-Origin': '*'
+    }
+  },
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  require('tailwindcss'),
+                  require('autoprefixer')
+                ]
+              }
+            }
+          }
+        ]
+      }
+    ]
   },
   plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    }),
     new ModuleFederationPlugin({
       name: 'commons',
       filename: 'remoteEntry.js',
       exposes: {
-        './Utils': './src/index.js',
+        './Card': './src/components/Card',
+        './Utils': './src/utils/index',
+        './Components': './src/components/index'
       },
       shared: {
         vue: {
           singleton: true,
-          requiredVersion: '^2.6.14',
-        },
-      },
-    }),
-  ],
+          eager: false,
+          requiredVersion: '^2.6.14'
+        }
+      }
+    })
+  ]
 };
